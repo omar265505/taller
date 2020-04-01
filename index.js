e3ecb95aeace6dab529d5b4a6a07d6230c81d255
omar265505 <61068032+omar265505@users.noreply.github.com>
2020-03-31 19:30:44 -0600

    nuevo

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const express=require("express");
 const app =express();
-app.get("/:name",(req,res,next)=>{
-    console.log(req.params.name);
-    res.send("bienvenido "+req.params.name);
-    })
-app.get("/:name",(req,res,next)=>{
-console.log(req.params.name);
-res.send("hola mundo "+req.params.name);
-res,status(200);
-})
+const { pokedex } =require('./pokedex.json');
 
-app.listen(3000,()=>{
-console.log("si corrio compa ");
+app.get("/",(req,res,next)=>{ 
+   res.status(200);
+    res.send("bienvenido al pokedexx");
+    });
+app.get("/pokemon",(req,res,next)=>{
+res.status(200);
+res.send(pokemon);
+});
+app.get("/pokemon/:id([0-9]{1,3})",(req,res,next)=>{
+    const id =req.params.id - 1;
+
+    if (id=> 0 && id <= 150){
+        res.status(200);
+      return  res.send(pokemon[req.params.id - 1]);
+    } 
+        res.status(404);
+        res.send("pokemon no encontado");
+    
+    });
+    app .get('/pokemon/:name'),(req,res,next)=>{
+        const name = req.params.name;
+        for(i=0;i<pokemon.length;i++){
+          if(pokemon[i].name==name){
+              res.status();
+              res.send(pokemon[i]);
+          }  
+          res.status(404);
+        res.send("pokemon no encontado");
+        }
+    }
+
+app.listen(process.env,PORT ||3000,()=>{
+console.log("server is runing ");
 });
